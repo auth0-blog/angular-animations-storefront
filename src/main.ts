742f3027b2ea2c6278893e112ff820c4c36f45c7
William Juan <williamjuan027@gmail.com>
2021-02-18 19:33:24 +0700

    add dependency for codesandbox

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// for code sandbox
+import 'core-js/proposals/reflect-metadata';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -8,5 +11,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
